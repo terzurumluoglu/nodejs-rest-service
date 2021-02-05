6fa0c1ad35a25197417fa3e2aa647b02d12d9dbd
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2021-02-05 23:53:03 +0300

    Post Controller edited!

--- src/api/v1/controllers/post.controller.js
+++ src/api/v1/controllers/post.controller.js
@@ -1,10 +1,26 @@
-const { versions } = require('../constants/version');
-const { successResponse } = require('../../helpers/response');
+const { version } = require('../constants/version');
+const { successResponse } = require('../utils/successResponse');
+const { postService } = require('../services');
+const ErrorResponse = require('../utils/errorResponse');
 
 const getAllPosts = (req, res, next) => {
-  const data = {};
-  const result = successResponse(versions.v1, 200, 'Get All Posts', data);
+  const posts = postService.getAllPosts();
+  const result = successResponse(versions.v1, 200, 'Get All Posts', posts);
   res.status(result.statusCode).json(result);
 };
 
-module.exports = { getAllPosts };
+const getPostById = (req, res, next) => {
+  const postId = +req.params.postId;
+  const post = postService.getPostById(postId);
+
+  if (!post) {
+    return next(
+      new ErrorResponse(version.v1, `Post not found id of ${postId}`, 404)
+    );
+  }
+
+  const result = successResponse(version.v1, 200, 'Get Single Post', post);
+  res.status(result.statusCode).json(result);
+};
+
+module.exports = { getAllPosts, getPostById };
