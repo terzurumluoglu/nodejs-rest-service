61cdfffa6091cb3e4bce9a72aae6fe63de7b3fbb
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2021-02-06 16:28:33 +0300

    Create Post ve Update Post eklendi.

--- src/api/v1/controllers/post.controller.js
+++ src/api/v1/controllers/post.controller.js
@@ -2,12 +2,13 @@ const { version } = require('../constants/version');
 const { successResponse } = require('../utils/successResponse');
 const { postService } = require('../services');
 const ErrorResponse = require('../utils/errorResponse');
+const { asyncHandler } = require('../middlewares/async');
 
-const getAllPosts = (req, res, next) => {
-  const posts = postService.getAllPosts();
-  const result = successResponse(versions.v1, 200, 'Get All Posts', posts);
+const getAllPosts = asyncHandler(async (req, res, next) => {
+  const posts = await postService.getAllPosts();
+  const result = successResponse(version.v1, 200, 'Get All Posts', posts);
   res.status(result.statusCode).json(result);
-};
+});
 
 const getPostById = (req, res, next) => {
   const postId = +req.params.postId;
@@ -23,4 +24,19 @@ const getPostById = (req, res, next) => {
   res.status(result.statusCode).json(result);
 };
 
-module.exports = { getAllPosts, getPostById };
+const createPost = asyncHandler(async (req, res, next) => {
+  const post = await postService.createPost(req.body);
+  const result = successResponse(version.v1, 200, 'Create a new Post', post);
+  res.status(result.statusCode).json(result);
+});
+
+const updatePost = asyncHandler(async (req, res, next) => {
+  const post = await postService.updatePost(req.params.postId, req.body, next);
+  if (!post) {
+    return next(new ErrorResponse(version.v1, `Not Found`, 404));
+  }
+  const result = successResponse(version.v1, 200, 'Updated Post', post);
+  res.status(result.statusCode).json(result);
+});
+
+module.exports = { getAllPosts, getPostById, createPost, updatePost };
