16fd824cea5f1cfdb42696f5b611e5907ea4776b
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2021-02-06 13:42:31 +0300

    Update tag.controller.js

--- src/api/v1/controllers/tag.controller.js
+++ src/api/v1/controllers/tag.controller.js
@@ -5,13 +5,7 @@ const { asyncHandler } = require('../middlewares/async');
 const ErrorResponse = require('../utils/errorResponse');
 
 const createTag = asyncHandler(async (req, res, next) => {
-  const { name } = req.body;
-
   const tag = await tagService.createTag(req.body);
-  if (!tag) {
-    return next(new ErrorResponse(version.v1, 'Hata', 500));
-  }
-
   const result = successResponse(version.v1, 200, 'Create a new Tag', tag);
   res.status(result.statusCode).json(result);
 });
