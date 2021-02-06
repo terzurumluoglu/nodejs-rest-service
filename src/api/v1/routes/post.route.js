5130886d11567ddea6712033da2f6d34fb116cf1
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2021-02-06 16:26:57 +0300

    Post Route kısaltıldı.

--- src/api/v1/routes/post.route.js
+++ src/api/v1/routes/post.route.js
@@ -2,7 +2,14 @@ var express = require('express');
 const router = express.Router({ mergeParams: true });
 const { postController } = require('../controllers');
 
-router.route('/').get(postController.getAllPosts);
-router.route('/:postId').get(postController.getPostById);
+router
+  .route('/')
+  .get(postController.getAllPosts)
+  .post(postController.createPost);
+
+router
+  .route('/:postId')
+  .get(postController.getPostById)
+  .put(postController.updatePost);
 
 module.exports = router;
