b7500ae0ea05622105df5ac2cef83a60faaf1287
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2021-02-06 16:26:41 +0300

    Tag route kısaltıldı.

--- src/api/v1/routes/tag.route.js
+++ src/api/v1/routes/tag.route.js
@@ -2,8 +2,6 @@ var express = require('express');
 const router = express.Router({ mergeParams: true });
 const { tagController } = require('../controllers');
 
-router.route('/').get(tagController.getAllTags);
-
-router.route('/').post(tagController.createTag);
+router.route('/').get(tagController.getAllTags).post(tagController.createTag);
 
 module.exports = router;
