31c7aed6fa780558421ed730daf058893ab468dc
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2021-02-06 16:27:50 +0300

    Tag için Required eklendi.

--- src/api/v1/models/tag.model.js
+++ src/api/v1/models/tag.model.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 const { collection } = require('../constants/collections');
 
-const TagSchema = mongoose.Schema({
-  name: String,
+const TagSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please Add Tag Name'],
+  },
 });
 
 module.exports = mongoose.model(collection.tag.name, TagSchema);
