2a812ae8b9265294de7dbf8f5ae76d65e1fad259
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2021-02-06 13:58:15 +0300

    Get All Tags Route

--- src/api/v1/routes/tag.route.js
+++ src/api/v1/routes/tag.route.js
@@ -2,6 +2,8 @@ var express = require('express');
 const router = express.Router({ mergeParams: true });
 const { tagController } = require('../controllers');
 
+router.route('/').get(tagController.getAllTags);
+
 router.route('/').post(tagController.createTag);
 
 module.exports = router;
