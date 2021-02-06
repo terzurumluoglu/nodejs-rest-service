31dd21bb43c378f53ead4b420878061f1da9147b
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2021-02-06 16:28:09 +0300

    Error Handler' a ValidationError eklendi.

--- src/api/v1/middlewares/errorHandler.js
+++ src/api/v1/middlewares/errorHandler.js
@@ -1,6 +1,14 @@
+const ErrorResponse = require('../utils/errorResponse');
+const { version } = require('../constants/version');
+
 exports.errorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
+
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(version.v1, message, 400);
+  }
 
   res.status(error.statusCode || 500).json({
     version: error.version,
