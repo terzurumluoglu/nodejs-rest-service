dc004dcbd1dbb49cf685ddc239544ff96b04d8e3
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2021-02-06 13:58:01 +0300

    Get All Tags Method addded and async handler removed

--- src/api/v1/services/tag.service.js
+++ src/api/v1/services/tag.service.js
@@ -2,15 +2,22 @@ const TagSchema = require('../models/tag.model');
 const { asyncHandler } = require('../middlewares/async');
 
 /**
+ * @desc    Send All Tag
+ *
+ * @returns {Array} tags
+ */
+const getAllTags = () => {
+  return TagSchema.find();
+};
+
+/**
  * @desc    Send a new Tag
  *
  * @param   {string} name
  * @returns {object} tag
  */
-const createTag = asyncHandler(async (name) => {
-  const tag = await TagSchema.create(name);
-  console.log(tag);
-  return tag;
-});
+const createTag = (name) => {
+  return TagSchema.create(name);
+};
 
-module.exports = { createTag };
+module.exports = { getAllTags, createTag };
