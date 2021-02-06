76e5017515fb47286d51413d600d686d4c5bcb75
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2021-02-06 13:58:45 +0300

    Get All Tags added on Controller

--- src/api/v1/controllers/tag.controller.js
+++ src/api/v1/controllers/tag.controller.js
@@ -4,10 +4,16 @@ const { version } = require('../constants/version');
 const { asyncHandler } = require('../middlewares/async');
 const ErrorResponse = require('../utils/errorResponse');
 
+const getAllTags = asyncHandler(async (req, res, next) => {
+  const tags = await tagService.getAllTags();
+  const result = successResponse(version.v1, 200, 'Get All Tags', tags);
+  res.status(result.statusCode).json(result);
+});
+
 const createTag = asyncHandler(async (req, res, next) => {
   const tag = await tagService.createTag(req.body);
   const result = successResponse(version.v1, 200, 'Create a new Tag', tag);
   res.status(result.statusCode).json(result);
 });
 
-module.exports = { createTag };
+module.exports = { getAllTags, createTag };
